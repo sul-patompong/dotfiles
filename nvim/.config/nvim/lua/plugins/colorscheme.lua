f25781256fab33936305e15262750eeb53d365f9
Patompong Sulsaksakul <patompong.beam@gmail.com>
2024-06-29 20:05:33 +0700

    add nvim

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  -- { "npxbr/gruvbox.nvim", name = "gruvbox", priority = 1000 },
+  -- { "savq/melange-nvim" },
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
