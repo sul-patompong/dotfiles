73015be7ac96cb75a3c4082406f2182638251d9f
Patompong Sulsaksakul <patompong.beam@gmail.com>
2024-06-30 20:27:41 +0700

    config transparent for gruvbox theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,14 @@
 return {
   -- { "npxbr/gruvbox.nvim", name = "gruvbox", priority = 1000 },
   -- { "savq/melange-nvim" },
-  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      transparent_mode = true,
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
