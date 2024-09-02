3b3e632904ad5acdcdb9d6987acda1011d14b1dd
Patompong Sulsaksakul <patompong.beam@gmail.com>
2024-09-02 21:00:11 +0700

    update picom and wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,9 +3,10 @@ local config = {}
 
 config.color_scheme = "Gruvbox Dark (Gogh)"
 config.font = wezterm.font("Iosevka Nerd Font")
-config.font_size = 16
+config.font_size = 13
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.8
 
 return config
