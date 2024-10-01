3712f10b4edc6669229eddeebfca07af000dd44a
Patompon (Beam) Sulsaksakul <patompong.sulsaksakul@7peakssoftware.com>
2024-10-01 23:08:31 +0700

    update and add zed config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,22 @@
 return {
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = { transparent_mode = true } },
   {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.gruvbox_material_transparent_background = 2
-      vim.g.gruvbox_material_enable_italic = true
-      vim.g.gruvbox_material_menu_selection_background = "red"
-      vim.cmd.colorscheme("gruvbox-material")
-    end,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
   },
+  -- {
+  --   "sainnhe/gruvbox-material",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     -- Optionally configure and load the colorscheme
+  --     -- directly inside the plugin declaration.
+  --     vim.g.gruvbox_material_transparent_background = 2
+  --     vim.g.gruvbox_material_enable_italic = true
+  --     vim.g.gruvbox_material_menu_selection_background = "red"
+  --     vim.cmd.colorscheme("gruvbox-material")
+  --   end,
+  -- },
 }
