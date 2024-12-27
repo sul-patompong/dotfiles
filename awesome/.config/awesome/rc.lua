c30cc86cbd27c9387daed6dda6939c36aafd14c1
Patompong Sulsaksakul <patompong.beam@gmail.com>
2024-12-27 23:52:19 +0700

    change default layout

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -91,7 +91,7 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 -- Table of layouts to cover with awful.layout.inc, order matters.
 tag.connect_signal("request::default_layouts", function()
 	awful.layout.append_default_layouts({
-		awful.layout.suit.floating,
+		-- awful.layout.suit.floating,
 		awful.layout.suit.tile,
 		-- awful.layout.suit.tile.left,
 		-- awful.layout.suit.tile.bottom,
@@ -100,8 +100,8 @@ tag.connect_signal("request::default_layouts", function()
 		-- awful.layout.suit.fair.horizontal,
 		-- awful.layout.suit.spiral,
 		-- awful.layout.suit.spiral.dwindle,
-		-- awful.layout.suit.max,
-		-- awful.layout.suit.max.fullscreen,
+		awful.layout.suit.max,
+		awful.layout.suit.max.fullscreen,
 		-- awful.layout.suit.magnifier,
 		-- awful.layout.suit.corner.nw,
 	})
