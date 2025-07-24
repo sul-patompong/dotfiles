741b4cc472d371a46427198f5fb81b68815781ff
Patompong Sulsaksakul <patompong.beam@gmail.com>
2025-07-24 23:33:10 +0700

    feat: update theme set to melange

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,20 @@
 return {
   -- { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = { transparent_mode = true } },
   -- { "rebelot/kanagawa.nvim", opts = { transparent = true, theme = "dragon" } },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = { transparent_background = true } },
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = { transparent_background = true } },
+  {
+    "savq/melange-nvim",
+    name = "melange",
+    priority = 1000,
+    config = function()
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "melange",
     },
   },
 }
