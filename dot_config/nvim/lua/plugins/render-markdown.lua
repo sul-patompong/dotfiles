6b07654887d97595497785b1f73b2fae68b951c0
Patompong Sulsaksakul <patompong.beam@gmail.com>
2025-08-09 13:52:19 +0700

    feat: add obsidian config for neovim

---
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,15 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  file_types = { "markdown", "vimwiki" },
+  -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+
+  config = function()
+    -- require("obsidian").opts.ui.enable = false
+    -- vim.api.nvim_buf_clear_namespace(0, vim.api.nvim_get_namespaces()["ObsidianUI"], 0, -1)
+    require("render-markdown").setup({})
+  end,
+}
