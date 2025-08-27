e2f2e1597c60735a22bc5a502fd7cedf27a37313
Patompong Sulsaksakul <patompong.beam@gmail.com>
2025-08-27 08:53:29 +0700

    feat: update color schema

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,19 +2,39 @@ return {
   -- { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = { transparent_mode = true } },
   -- { "rebelot/kanagawa.nvim", opts = { transparent = true, theme = "dragon" } },
   -- { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = { transparent_background = true } },
+  -- {
+  --   "savq/melange-nvim",
+  --   name = "melange",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  --     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  --   end,
+  -- },
   {
-    "savq/melange-nvim",
-    name = "melange",
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
     priority = 1000,
-    config = function()
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end,
+    opts = {
+      transparent = true,
+    },
   },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "melange",
+      colorscheme = "cyberdream",
     },
   },
 }
