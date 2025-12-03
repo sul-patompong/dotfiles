0d674ce4a4f94c74ca69053386d4e805006e7e49
Patompong Sulsaksakul <patompong.beam@gmail.com>
2025-12-04 00:03:48 +0700

    feat: update tmux and vim

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,11 +3,6 @@
 -- Add any additional keymaps here
 local map = LazyVim.safe_keymap_set
 
-map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "window left" })
-map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "window right" })
-map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "window down" })
-map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "window up" })
-
 map("n", "<C-u>", "<C-u>zz", { desc = "scroll up" })
 map("n", "<C-d>", "<C-d>zz", { desc = "scroll down" })
 
