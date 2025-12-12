fa68b54745ab48febf8212cfb2ad313fda62799e
Patompong Sulsaksakul <patompong.beam@gmail.com>
2025-12-12 23:30:53 +0700

    feat: hypr

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,4 +5,18 @@ end
 export PATH="$PATH:/home/pbeam1992/.dotnet/tools"
 export PATH="$HOME/.local/bin:$PATH"
 
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
 starship init fish | source
