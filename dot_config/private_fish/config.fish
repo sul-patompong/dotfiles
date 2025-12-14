244012ac458ec73dc00a74a7a504b586bcf06c50
Patompong Sulsaksakul <patompong.beam@gmail.com>
2025-12-14 23:40:44 +0700

    feat: update dotnet config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -10,10 +10,22 @@ if status is-interactive
     end
 end
 
-export PATH="$PATH:/home/pbeam1992/.dotnet/tools"
-export PATH="$HOME/.local/bin:$PATH"
+# .NET configuration
+set -gx DOTNET_ROOT /usr/share/dotnet
+fish_add_path ~/.dotnet/tools
 
-# asdf (installed via pacman on Arch)
-fish_add_path ~/.asdf/shims
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 starship init fish | source
