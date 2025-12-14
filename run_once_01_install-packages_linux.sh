486df3353b728225310d5ed2033959aab7cded8e
Patompong Sulsaksakul <patompong.beam@gmail.com>
2025-12-14 15:24:34 +0700

    feat: add font and 1password, brave

--- run_once_01_install-packages_linux.sh
+++ run_once_01_install-packages_linux.sh
@@ -27,6 +27,9 @@ packages=(
   "eza"
   "lazygit"
   "ttf-iosevka-nerd" # Added for IDE and terminal font
+  "ttf-sarabun" # Added as per user request
+  "1password"
+  "brave-bin"
 )
 
 # Filter out already installed packages
