24b08f27990a6be318dbd1fb5f662c323a6905d8
Patompong Sulsaksakul <patompong.beam@gmail.com>
2025-12-14 20:00:34 +0700

    feat: update theme for neovim

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,75 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
+    "savq/melange-nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Apply transparency after colorscheme loads
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+          local groups = {
+            "Normal",
+            "NormalNC",
+            "NormalFloat",
+            "FloatBorder",
+            "FloatTitle",
+            "SignColumn",
+            "EndOfBuffer",
+            "MsgArea",
+            "VertSplit",
+            "WinSeparator",
+            "StatusLine",
+            "StatusLineNC",
+            "TabLine",
+            "TabLineFill",
+            "LineNr",
+            "CursorLineNr",
+            "Folded",
+            "FoldColumn",
+            "NonText",
+            "SpecialKey",
+            "Pmenu",
+            "PmenuSbar",
+            -- NeoTree
+            "NeoTreeNormal",
+            "NeoTreeNormalNC",
+            "NeoTreeEndOfBuffer",
+            "NeoTreeWinSeparator",
+            -- Telescope
+            "TelescopeNormal",
+            "TelescopeBorder",
+            "TelescopePromptNormal",
+            "TelescopePromptBorder",
+            "TelescopeResultsNormal",
+            "TelescopeResultsBorder",
+            "TelescopePreviewNormal",
+            "TelescopePreviewBorder",
+            -- WhichKey
+            "WhichKeyFloat",
+            -- Notify
+            "NotifyBackground",
+            -- Noice
+            "NoiceCmdlinePopup",
+            "NoiceCmdlinePopupBorder",
+          }
+
+          for _, group in ipairs(groups) do
+            local ok, hl = pcall(vim.api.nvim_get_hl, 0, { name = group })
+            if ok then
+              hl.bg = "NONE"
+              hl.ctermbg = "NONE"
+              vim.api.nvim_set_hl(0, group, hl)
+            else
+              vim.api.nvim_set_hl(0, group, { bg = "NONE", ctermbg = "NONE" })
+            end
+          end
+        end,
+      })
+
+      vim.cmd.colorscheme("melange")
+    end,
   },
+
+  { "LazyVim/LazyVim", opts = { colorscheme = "melange" } },
 }
