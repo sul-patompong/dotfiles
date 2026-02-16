fbe15b95885420cfe7e716c1b8a94f36990e9458
Patompong Sulsaksakul <patompong.beam@gmail.com>
2026-02-16 15:22:45 +0700

    feat: change theme to everforest

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,75 +1,22 @@
 return {
   {
-    "savq/melange-nvim",
+    "neanias/everforest-nvim",
+    version = false,
     lazy = false,
     priority = 1000,
     config = function()
-      -- Apply transparency after colorscheme loads
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "*",
-        callback = function()
-          local groups = {
-            "Normal",
-            "NormalNC",
-            "NormalFloat",
-            "FloatBorder",
-            "FloatTitle",
-            "SignColumn",
-            "EndOfBuffer",
-            "MsgArea",
-            "VertSplit",
-            "WinSeparator",
-            "StatusLine",
-            "StatusLineNC",
-            "TabLine",
-            "TabLineFill",
-            "LineNr",
-            "CursorLineNr",
-            "Folded",
-            "FoldColumn",
-            "NonText",
-            "SpecialKey",
-            "Pmenu",
-            "PmenuSbar",
-            -- NeoTree
-            "NeoTreeNormal",
-            "NeoTreeNormalNC",
-            "NeoTreeEndOfBuffer",
-            "NeoTreeWinSeparator",
-            -- Telescope
-            "TelescopeNormal",
-            "TelescopeBorder",
-            "TelescopePromptNormal",
-            "TelescopePromptBorder",
-            "TelescopeResultsNormal",
-            "TelescopeResultsBorder",
-            "TelescopePreviewNormal",
-            "TelescopePreviewBorder",
-            -- WhichKey
-            "WhichKeyFloat",
-            -- Notify
-            "NotifyBackground",
-            -- Noice
-            "NoiceCmdlinePopup",
-            "NoiceCmdlinePopupBorder",
-          }
-
-          for _, group in ipairs(groups) do
-            local ok, hl = pcall(vim.api.nvim_get_hl, 0, { name = group })
-            if ok then
-              hl.bg = "NONE"
-              hl.ctermbg = "NONE"
-              vim.api.nvim_set_hl(0, group, hl)
-            else
-              vim.api.nvim_set_hl(0, group, { bg = "NONE", ctermbg = "NONE" })
-            end
-          end
+      require("everforest").setup({
+        background = "hard",
+        transparent_background_level = 2,
+        on_highlights = function(hl, palette)
+          hl.NormalFloat = { bg = palette.none }
+          hl.FloatBorder = { bg = palette.none }
         end,
       })
 
-      vim.cmd.colorscheme("melange")
+      vim.cmd.colorscheme("everforest")
     end,
   },
 
-  { "LazyVim/LazyVim", opts = { colorscheme = "melange" } },
+  { "LazyVim/LazyVim", opts = { colorscheme = "everforest" } },
 }
