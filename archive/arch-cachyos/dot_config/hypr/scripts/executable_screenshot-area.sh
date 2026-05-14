d173c88c9b80447a7fda1b3f6f3436c6aa6cca53
Patompong Sulsaksakul <patompong.beam@gmail.com>
2026-05-14 11:32:16 +0700

    feat: remove archive

--- archive/arch-cachyos/dot_config/hypr/scripts/executable_screenshot-area.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Capture selected area and open in satty for annotation
-
-grim -g "$(slurp)" - | satty --filename - --output-filename ~/Pictures/Screenshots/screenshot-$(date +%Y-%m-%d_%H-%M-%S).png
--- archive/arch-cachyos/dot_config/hypr/scripts/executable_screenshot-fullscreen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Capture entire screen and open in satty for annotation
-
-grim - | satty --filename - --fullscreen --output-filename ~/Pictures/Screenshots/screenshot-$(date +%Y-%m-%d_%H-%M-%S).png
